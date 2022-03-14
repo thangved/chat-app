213d6f93d178ef0e2b1fadeef506814780507ed8
thangved <thangcreation@gmail.com>
2022-03-14 18:39:13 +0700

    initial

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const users: string[] = [];
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+	},
+});
+
+io.on("connection", (socket) => {
+	users.push(socket.id);
+
+	io.emit("users", { users, join: socket.id });
+
+	socket.on("message", (message) => {
+		io.emit("message", message);
+	});
+	socket.on("disconnecting", () => {
+		const leave = users.pop();
+		io.emit("users", {
+			users,
+			leave: leave,
+		});
+	});
+});
+
+httpServer.listen(process.env.PORT || 3000);
